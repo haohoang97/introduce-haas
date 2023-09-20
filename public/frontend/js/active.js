bb4d4df3af57b0769a2fba26a33abb6e21dd4c25
haohoanggt97 <haohoanggt97@gmail.com>
2023-09-20 16:18:01 +0700

    Change name shop

--- public/frontend/js/active.js
+++ public/frontend/js/active.js
@@ -1,8 +1,8 @@
 /* =====================================
-Template Name: Eshop
+Template Name: Naganext
 Author Name: Naimur Rahman
 Author URI: http://www.wpthemesgrid.com/
-Description: Eshop - eCommerce HTML5 Template.
+Description: Naganext - eCommerce HTML5 Template.
 Version:1.0
 ========================================*/
 /*=======================================
